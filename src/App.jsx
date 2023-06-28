d477927336ca7619d56516c91d87c96af25060f0
jlvilasoler <jlvilasoler@hotmail.com>
2023-06-27 23:48:48 -0300

    se saca el toastify en otros lugares

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import Contacto from "./components/Contacto/Contacto";
 import Registro from "./components/Registro/Registro";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import "./App.css";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import MenuInicio from "./components/MenuInicio/MenuInicio";
 import { BrowserRouter , Routes , Route, Navigate} from "react-router-dom";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
